7808589071e8f884ae442a334f5d2d30f5731f94
PriyanshuRanjann <14priyanshuranjan@gmail.com>
2026-01-15 21:11:23 +0530

    refactor: streamline read_parsed_json function and remove unused get_parsed_json

--- helper_function/chunker.py
+++ helper_function/chunker.py
@@ -1,19 +1,18 @@
 import json
-
+from .pdf_parser import save_pdf_pages_to_json
 
 INPUT_DIR = "output/parsed_pdf"
 OUTPUT_DIR = "output/chunks"
 MAX_CHARS_PER_CHUNK = 600
 MIN_CHARS_PER_CHUNK = 200
 
-def get_parsed_json():
-    pass
 
-
-def read_parsed_json(path):
+def read_parsed_json(path,input):
     """Read parsed JSON file and return its content."""
     try:
+        save_pdf_pages_to_json(input)
         with open(path, 'r', encoding='utf-8') as f:
+            print("Successfully read the parsed JSON file.")
             return json.load(f)
     except Exception as e:
         print(f"Failed to read: {path}: {e}")
