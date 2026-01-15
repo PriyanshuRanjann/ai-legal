da669a8adcaea931a7c283a479258018268c480c
PriyanshuRanjann <14priyanshuranjan@gmail.com>
2026-01-15 21:19:42 +0530

    fix: ensure JSON file is created if it doesn't exist before.

--- helper_function/chunker.py
+++ helper_function/chunker.py
@@ -1,3 +1,4 @@
+import os
 import json
 from .pdf_parser import save_pdf_pages_to_json
 
@@ -10,7 +11,8 @@ MIN_CHARS_PER_CHUNK = 200
 def read_parsed_json(path,input):
     """Read parsed JSON file and return its content."""
     try:
-        save_pdf_pages_to_json(input)
+        if not os.path.exists(path):
+            save_pdf_pages_to_json(input)
         with open(path, 'r', encoding='utf-8') as f:
             print("Successfully read the parsed JSON file.")
             return json.load(f)
@@ -18,3 +20,7 @@ def read_parsed_json(path,input):
         print(f"Failed to read: {path}: {e}")
         return None
 
+# if __name__ == "__main__":
+#     input = r"input/the-state-of-ai-how-organizations-are-rewiring-to-capture-value_final.pdf"
+#     output_path = os.path.join(INPUT_DIR, "the-state-of-ai-how-organizations-are-rewiring-to-capture-value_final_page_0.json")
+#     read_parsed_json(output_path,input)
